a5db13148aefbfa071dc3d700555b7ba134338e3
Wende Lu <wendelu1991@gmail.com>
2017-12-14 08:39:08 +0800

    foo and wisdom of life

--- play/foo.rb
+++ play/foo.rb
@@ -1,13 +1,38 @@
-def c
-  caller
+require 'ostruct'
+
+$a = 0
+
+def start
+  $a += 1
+  OpenStruct.new(key: "start", value: $a)
 end
 
-def b
-  c
+def stop
+  $a += 1
+  OpenStruct.new(key: "stop", value: $a)
 end
 
-def a
-  b
-end
+a = [stop, start, stop, start, start, stop, stop, start, start, start, stop, start]
 
-p a
+# 2, 3, 4, 6, 8, 11
+
+
+tmp = "stop"
+b = []
+a.each do |e|
+  if e.key != tmp
+    b << e
+    tmp = e.key
+  end
+end
+b.pop if b.last.key == 'start'
+
+# >> pp b
+# [#<OpenStruct key="start", value=2>,
+#  #<OpenStruct key="stop", value=3>,
+#  #<OpenStruct key="start", value=4>,
+#  #<OpenStruct key="stop", value=6>,
+#  #<OpenStruct key="start", value=8>,
+#  #<OpenStruct key="stop", value=11>]
+
+
